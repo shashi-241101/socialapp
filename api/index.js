222ca65f8949a1253ebeb06291a1ff8b6613ec5d
Arpit <arpitagrawal2402@gmail.com>
2023-09-06 13:13:06 +0530

    db connection fixed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
@@ -8,18 +6,14 @@ const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const router = express.Router();
 const path = require("path");
+const connectDB = require("./connect");
+const app = express();
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
+connectDB();
+
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
